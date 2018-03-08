67f2b52f8a301ce54eb7365a8fd37af49d76c3e2
Ismael <ismatuner@gmail.com>
2018-03-08 20:58:48 +0100

    First code

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+function love.load()
+
+end
+
 function love.draw()
 	love.graphics.print("Hello World!", 400, 300)
+end
+
+function love.update(dt)
+
+end
+
+function love.quit()
+  print("Gracias por jugar! Vuelve pronto!")
 end
